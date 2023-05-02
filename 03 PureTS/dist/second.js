a1fd4b19728d5981d558b56887a1eeb49d84328c
abidalili <abidalili1995@gmail.com>
2023-05-03 00:29:58 +0300

    updated

--- 03 PureTS/dist/second.js
+++ 03 PureTS/dist/second.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("object");
+// interface TakePhoto {
+//   cameraMode: string;
+//   filter: string;
+//   burst: number;
+// }
+// interface Story {
+//   createStory(): void;
+// }
+// class Instagram implements TakePhoto, story {
+//   constructor(
+//     public cameraMode: string,
+//     public filter: string,
+//     public burst: number
+//   ) {}
+// }
+// //more than given value is ok... bt can not be less
+// class Youtube implements TakePhoto {
+//   constructor(
+//     public cameraMode: string,
+//     public filter: string,
+//     public burst: number,
+//     public short: string
+//   ) {}
+//   createStory(): void {
+//     console.log("story was created");
+//   }
+// }
